8ae9f192e751e9a73d5d1d342632c3e8f3cf160c
JoGMG <joattah3@gmail.com>
2025-06-14 05:38:36 +0100

    fix cors error

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin:
+      process.env.FRONTEND_URL ||
+      'https://qrcode-certificate-generator.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
